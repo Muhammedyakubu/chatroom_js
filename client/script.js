6a3765c01280bcf0745833e7eeaa39742ff2d6f1
Muhammed Yakubu <72368510+Muhammedyakubu@users.noreply.github.com>
2022-01-13 21:38:14 -0500

    completed basic chat functionality

--- client/script.js
+++ client/script.js
@@ -1,28 +1,67 @@
 const socket = io('http://localhost:8080');
 
+const messageContainer = document.querySelector('.messages-container')
+
+let username = prompt('What is your name?')
+while (!username) username = prompt('What is your name?')
+
+/*window.onload = () => {
+    const auth = checkIfLoggedIn()
+    if (!auth) {
+        setUsername()
+    }
+} */
+
+
+
+function alertAction (user, action) {
+    const el = document.createElement('h5')
+    el.innerHTML = (user == username? "You" : user) + " " + action;
+    console.log(el.innerHTML)
+    el.classList.add('alert','new-user')
+    messageContainer.append(el)
+    el.scrollIntoView()
+}
+
+function appendMessage(chat) {
+    const el = document.createElement('div')
+    el.innerHTML = 
+    `<h5>${chat.sentBy == username? "You": chat.sentBy}</h5>
+    <p>${chat.content}</p>
+    <h6>${chat.timeSent}</h6>`
+    el.classList.add("message")
+    el.classList.add( chat.sentBy === username ? "sent": "received")
+    messageContainer.appendChild(el)
+    el.scrollIntoView()
+}
+
 socket.on("connect", () => {
-    console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+    console.log(socket.id, username); 
+    socket.emit('new-user', username)
   });
 
 socket.on('message', packet => {
-    const recv = JSON.parse(packet)
-    const el = document.createElement('li');
-    el.classList.add("message")
-    el.classList.add( socket.id === recv.id ? "sent": "received")
-    el.innerHTML = recv.id.substr(0,2) + ": " + recv.message;
-    document.querySelector('.messages-container').appendChild(el)
-
+    const chat = JSON.parse(packet)
+    appendMessage(chat)
 });
+
+socket.on('user-connected', user => {
+    alertAction(user, 'joined')
+})
+
+socket.on('user-disconnected', user => {
+    alertAction(user, 'left')
+})
 
 document.querySelector('#send-button').onclick = (e) => {
     e.preventDefault()
     const input = document.querySelector('#message-input');
-    socket.emit('message', input.value)
+    const chat = {
+        content: input.value,
+        sentBy: username,
+        timeSent: new Date
+    }
+    socket.emit('message', JSON.stringify(chat))
     input.value = ""
 }
 
-/* document.querySelector("#set-name").onclick = (e) => {
-    e.preventDefault()
-    const input = document.querySelector('#name-input')
-    socket.username = input.value
-} */
--- server/server.js
+++ server/server.js
@@ -1,20 +1,43 @@
-const http = require('http').createServer();
 
+/* const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server, {
+    cors: { origin: "*" }
+}); */
+
+const http = require('http').createServer();
 const io = require('socket.io')(http, {
     cors: { origin: "*" }
 });
 
+const users = {}
+
 io.on('connection', (socket) => {
     console.log('a user connected', socket.id);
 
-    socket.on('message', (message) =>     {
-        console.log(message);
-        const send = JSON.stringify({
-            id: socket.id,
-            message
-        })
-        io.emit('message', send);   
+    socket.on('message', (chat) =>     {
+        console.log(JSON.parse(chat));
+        io.emit('message', chat);   
     });
+
+    socket.on('new-user', user => {
+        io.emit('user-connected', user);
+        users[socket.id] = user
+        console.log("new user: ", user)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('user-disconnected', users[socket.id])
+    })
 });
 
 http.listen(8080, () => console.log('listening on http://localhost:8080') );
+
+//creating storing chats
+/* const mongoose = require('mongoose')
+const chatSchema = new mongoose.Schema({
+    content: String,
+    sentBy: String,
+    timeSent: String
+}); */
