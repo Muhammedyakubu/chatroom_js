3f7e27f94eaffe6a8f0e22af1819e3d305171d90
Muhammed Yakubu <72368510+Muhammedyakubu@users.noreply.github.com>
2022-01-17 01:32:54 -0500

    added a detailed plan for the rest of the project

--- client/script.js
+++ client/script.js
@@ -1,67 +1,98 @@
-const socket = io('http://localhost:8080');
+const SERVER = "http://localhost:8080"
+const socket = io(SERVER);
 
-const messageContainer = document.querySelector('.messages-container')
-
-let username = prompt('What is your name?')
-while (!username) username = prompt('What is your name?')
-
-/*window.onload = () => {
-    const auth = checkIfLoggedIn()
-    if (!auth) {
-        setUsername()
-    }
-} */
+const messageContainer = document.querySelector(".messages-container");
+const loginModal = document.querySelector("#login");
+const content = document.querySelector(".content");
+const signOut = document.querySelector("#sign-out");
 
 
+let users = [];
 
-function alertAction (user, action) {
-    const el = document.createElement('h5')
-    el.innerHTML = (user == username? "You" : user) + " " + action;
-    console.log(el.innerHTML)
-    el.classList.add('alert','new-user')
-    messageContainer.append(el)
-    el.scrollIntoView()
+let username;
+window.onload = () => {
+	username = JSON.parse(localStorage.getItem("username"));
+	while (!username || username == "null") {
+		//check username
+		if (users.includes(username)) {
+			username = prompt(
+				"Username exsists!! Please choose something else"
+			);
+		} else if (username == "null") {
+			username = prompt(
+				"Invalid username!! Please choose something else"
+			);
+		} else {
+			username = prompt("Hey there! What's your name");
+		}
+		localStorage.setItem("username", JSON.stringify(username));
+	}
+};
+
+signOut.addEventListener("click", () => {
+	localStorage.setItem("username", null);
+	location.reload();
+});
+
+loginModal.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const input = document.querySelector("#name-input");
+	username = input.value;
+	loginModal.classList.toggle("hidden");
+	content.classList.toggle("hidden");
+});
+
+function login() {
+	loginModal.classList.toggle("hidden");
+	content.classList.add("hidden");
+}
+
+function alertAction(user, action) {
+	const el = document.createElement("h5");
+	el.innerHTML = (user == username ? "You" : user) + " " + action;
+	console.log(el.innerHTML);
+	el.classList.add("alert", "new-user");
+	messageContainer.append(el);
+	el.scrollIntoView();
 }
 
 function appendMessage(chat) {
-    const el = document.createElement('div')
-    el.innerHTML = 
-    `<h5>${chat.sentBy == username? "You": chat.sentBy}</h5>
+	const el = document.createElement("div");
+	el.innerHTML = `<h5>${chat.sentBy == username ? "You" : chat.sentBy}</h5>
     <p>${chat.content}</p>
-    <h6>${chat.timeSent}</h6>`
-    el.classList.add("message")
-    el.classList.add( chat.sentBy === username ? "sent": "received")
-    messageContainer.appendChild(el)
-    el.scrollIntoView()
+    <h6>${new Date(chat.timeSent).toLocaleTimeString()}</h6>`;
+	el.classList.add("message");
+	el.classList.add(chat.sentBy === username ? "sent" : "received");
+	messageContainer.appendChild(el);
+	el.scrollIntoView();
 }
 
 socket.on("connect", () => {
-    console.log(socket.id, username); 
-    socket.emit('new-user', username)
-  });
-
-socket.on('message', packet => {
-    const chat = JSON.parse(packet)
-    appendMessage(chat)
+	console.log(socket.id, username);
+	socket.emit("new-user", username);
 });
 
-socket.on('user-connected', user => {
-    alertAction(user, 'joined')
-})
+socket.on("message", (packet) => {
+	const chat = JSON.parse(packet);
+	appendMessage(chat);
+});
 
-socket.on('user-disconnected', user => {
-    alertAction(user, 'left')
-})
+socket.on("user-connected", (user) => {
+	alertAction(user, "joined");
+});
 
-document.querySelector('#send-button').onclick = (e) => {
-    e.preventDefault()
-    const input = document.querySelector('#message-input');
-    const chat = {
-        content: input.value,
-        sentBy: username,
-        timeSent: new Date
-    }
-    socket.emit('message', JSON.stringify(chat))
-    input.value = ""
-}
+socket.on("user-disconnected", (user) => {
+	alertAction(user, "left");
+});
 
+document.querySelector("#send-button").onclick = (e) => {
+	e.preventDefault();
+	const input = document.querySelector("#message-input");
+	const chat = {
+		content: input.value,
+		sentBy: username,
+		timeSent: Date.now().toString(),
+	};
+	socket.emit("message", JSON.stringify(chat));
+	input.value = "";
+};
--- server/server.js
+++ server/server.js
@@ -1,17 +1,33 @@
 
-/* const express = require('express')
-const app = express()
+/* 
 const server = require('http').Server(app)
 const io = require('socket.io')(server, {
     cors: { origin: "*" }
 }); */
+const express = require('express')
+const app = express()
+const http = require('http').Server(app);
+const io = require("socket.io")(http, {
+    cors: {
+      origin: "*"
+    }
+  });
+const port = process.env.PORT || 8080
+const path = require('path')
 
-const http = require('http').createServer();
-const io = require('socket.io')(http, {
-    cors: { origin: "*" }
-});
+app.set('view engine', 'ejs')
 
 const users = {}
+const activeUsers = ["Jay", "Shash", "Foo", "Bar"]
+
+/* app.get('/api/chat', (req, res) => {
+    res.status(200).send("Hello")
+    res.send(chats)
+}) */
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/../client', '/index.html'));
+});
 
 io.on('connection', (socket) => {
     console.log('a user connected', socket.id);
@@ -29,10 +45,11 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
         io.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
     })
 });
 
-http.listen(8080, () => console.log('listening on http://localhost:8080') );
+http.listen(port, () => console.log('listening on http://localhost:' + port) );
 
 //creating storing chats
 /* const mongoose = require('mongoose')
